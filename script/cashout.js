6bcf2bf62cfec8728c808c0f307755b8381a90a4
mahfuz-7148 <mrahman7148@gmail.com>
2025-02-27 16:59:41 +0600

    cash out updated

--- script/cashout.js
+++ script/cashout.js
@@ -2,19 +2,21 @@ document
   .getElementById("cashoutBtn")
   .addEventListener("click", function (event) {
     event.preventDefault();
-    const amount = document.getElementById("cashAmount").value;
-    const convertedAmount = Math.floor(amount);
-    const pinNumber = document.getElementById("cashOutpinNumber").value;
+    const cashAccountNumber =
+      document.getElementById("cashAccountNumber").value;
+    const cashAmount = document.getElementById("cashAmount").value;
+    const convertedAmount = Number(cashAmount);
+    const cashOutpinNumber = document.getElementById("cashOutpinNumber").value;
     const mainBalance = document.getElementById("mainBalance").innerText;
-    const convertedMainBalance = Math.floor(mainBalance);
-    if (amount && pinNumber) {
-      if (pinNumber === "6897") {
+    const convertedMainBalance = Number(mainBalance);
+    if (cashAccountNumber.length === 11) {
+      if (cashOutpinNumber === "6897") {
         const sum = convertedMainBalance - convertedAmount;
         document.getElementById("mainBalance").innerText = sum;
       } else {
         window.alert("enter the valid pin number");
       }
     } else {
-      window.alert("enter the amount");
+      window.alert("enter the valid Account Number");
     }
   });
